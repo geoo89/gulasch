97f18f53c05502cb36a58cb34e7625a6ca879007
unknown <dhoske@ate.(none)>
2012-06-08 19:33:13 +0200

    Correct rendering

--- render.lua
+++ render.lua
@@ -11,11 +11,20 @@ function render:add(img, x, y, z, brightness)
     self[#self + 1] = o
 end
 
--- Renders in correct order
+-- Renders in correct order and with right brightness
 function render:draw()
     self:sort()
     for i,v in ipairs(self) do
+        -- doubleplusungood way to adjust brightness 
+        gr.setColor(v.bright, v.bright, v.bright)
         gr.draw(v.inner, v.x, v.y)
+        
+        -- gr.setColorMode("modulate")
+        -- gr.setBlendMode("multiplicative")
+        -- gr.setColor(v.bright, v.bright, v.bright)
+        -- w = v.inner:getWidth()
+        -- h = v.inner:getHeight()
+        -- gr.rectangle("fill", v.x, v.y, v.x + w - 1, v.y + h - 1)
     end
     self={}
 end
