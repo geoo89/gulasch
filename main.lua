3f9aa32231df2b08c6b817356f892fb146e5c78d
unknown <dhoske@ate.(none)>
2012-06-10 19:03:21 +0200

    Fonts now support sizes

--- main.lua
+++ main.lua
@@ -57,6 +57,7 @@ function love.draw()
     
     text:print("x:"..(math.floor(player.cx*100)/100).." y:"..(math.floor(player.cy*100)/100))
     text:print(timer.getFPS())
+    text:print(timer.getFPS(), 100, 100, 50)
     text:print("Move crate to goal")
     if WON==true then 
         text:print("A winner is you!") 
--- text.lua
+++ text.lua
@@ -1,27 +1,49 @@
 require 'shortcut'
 
-text = {lines = {}}
+text = {lines_left = {}, lines_other = {}, fonts = {}}
 text.FONT_SIZE = 12
 text.LEADING = 1.2
 text.OFF_X = 10
 text.OFF_Y = 10
 
+-- Get font of respective size
+function text:getFont(size)
+    if not self.fonts[size] then
+        self.fonts[size] = gr.newFont(size)
+    end
+    
+    return self.fonts[size]
+end
+
 function text:init()
-    gr.setFont(gr.newFont(self.FONT_SIZE))
+    
 end
 
 -- Add text to render list
-function text:print(str, size)
-    self.lines[#self.lines + 1] = {str = str, size = size or self.FONT_SIZE}
+function text:print(str, x, y, size)
+    if x and y then
+        self.lines_other[#self.lines_other + 1] = {str = str, size = size or self.FONT_SIZE, x = x, y = y}
+    else
+        self.lines_left[#self.lines_left + 1] = {str = str, size = size or self.FONT_SIZE}
+    end
 end
 
 function text:draw()
     -- Now print lines in given sizes
     local x = text.OFF_X
     local y = text.OFF_Y
-    for i, v in ipairs(self.lines) do
-        gr.print(v.str, x, y, 0, v.size / self.FONT_SIZE)
+    for i, v in ipairs(self.lines_left) do
+        gr.setFont(self:getFont(v.size))
+        gr.print(v.str, x, y)
         y = y + v.size * self.LEADING
     end
-    self.lines = {}
+    
+    -- Now print arbitrary lines
+    for i, v in ipairs(self.lines_other) do
+        gr.setFont(self:getFont(v.size))
+        gr.print(v.str, v.x, v.y)
+    end
+    
+    self.lines_left = {}
+    self.lines_other = {}
 end
